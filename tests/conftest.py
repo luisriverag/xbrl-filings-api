d69b4a836f787f1c473170c849ec55133406e4d4
Lauri Salmela <lauri.m.salmela@gmail.com>
2023-09-15 12:58:48 +0300

    Fixed test config for responses mock URLs.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,12 +6,24 @@ Configuration of `pytest` library.
     `responses` version 0.23.3).
 """
 
+from pathlib import Path
+
 import pytest
 import responses
+
+MOCK_URL_DIR_NAME = '.mock_url_cache'
+
+mock_dir_path = Path(__file__).parent / MOCK_URL_DIR_NAME
+
+
+def _get_absolute_path(set_id):
+    file_path = mock_dir_path / f'{set_id}_mock.yaml'
+    return str(file_path)
 
 
 @pytest.fixture
 def api_responses():
     """Fixture for mocked 'api' responses."""
     with responses.RequestsMock() as rsps:
+        rsps._add_from_file(_get_absolute_path('api'))
         yield rsps
--- tests/unit/test_filings_api.py
+++ tests/unit/test_filings_api.py
@@ -1,30 +1,16 @@
-"""Define tests for root functions."""
+"""Define tests for query functions."""
 
 # SPDX-FileCopyrightText: 2023 Lauri Salmela <lauri.m.salmela@gmail.com>
 #
 # SPDX-License-Identifier: MIT
 
-from pathlib import Path
-
 import responses
 
 from xbrl_filings_api import GET_ONLY_FILINGS, filings_api
 
-MOCK_URL_DIR_NAME = '.mock_url_cache'
-
-mock_dir_path = Path(__file__).parent.parent / MOCK_URL_DIR_NAME
-
-
-def _get_absolute_path(set_id):
-    file_path = mock_dir_path / f'{set_id}_mock.yaml'
-    return str(file_path)
-
 
 class Test_get_filings:
-    @responses.activate
-    def test_fetch_asml22_country(s, api_responses):
-        api_responses._add_from_file(file_path=_get_absolute_path('api'))
-        # ASML Holding
+    def test_fetch_asml22(api_responses):
         fs = filings_api.get_filings(
             filters={'entity.identifier': '724500Y6DUVHQD6OXN27'},
             max_size=2,
@@ -33,6 +19,15 @@ class Test_get_filings:
         asml22_fxo = '724500Y6DUVHQD6OXN27-2022-12-31-ESEF-NL-0'
         asml22 = next((f for f in fs if f.filing_index == asml22_fxo), None)
         assert asml22 is not None, 'Requested filing was not returned'
+
+    def test_fetch_asml22_country(api_responses):
+        fs = filings_api.get_filings(
+            filters={'entity.identifier': '724500Y6DUVHQD6OXN27'},
+            max_size=2,
+            flags=GET_ONLY_FILINGS
+            )
+        asml22_fxo = '724500Y6DUVHQD6OXN27-2022-12-31-ESEF-NL-0'
+        asml22 = next((f for f in fs if f.filing_index == asml22_fxo), None)
         assert asml22.country == 'NL'
 
 # filings_api.to_sqlite
